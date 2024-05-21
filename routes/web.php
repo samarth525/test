6bc8392011112354adda2eb9ee33478a34126908
samarth525 <samarth.chaurasia@augurs.in>
2024-05-21 18:50:42 +0530

    test

--- routes/web.php
+++ routes/web.php
@@ -51,7 +51,7 @@ Route::get('/test29', function () {
     return "hello test";
 });
 Route::get('/test30', function () {
-    return "hello testtt";
+    return "hello test";
 });
 Route::get('/test31', function () {
     return "hello test";
